4001ce528bd44e597c2ef6124f1ee6c69b64c48a
genp <gen@cs.brown.edu>
2013-01-30 16:02:00 -0500

    version 4.5 of cdoersch code

---
+++ scene_classifier/makeDetectorFile.m
@@ -0,0 +1,8 @@
+% gen
+% this file generates the set of detectors discovered from a labeled dataset (i.e. 15 scene dataset)
+
+% i'm not sure how the ranking of the final detectors gets stored,
+% so this is just a best guess situation. I'm taking the detectors
+% from findetectors[] or batch/round(last).
+% I'm using the first 100 from each set of detectors. 
+
