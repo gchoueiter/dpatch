a88e5fc5a8d3d59487aa6b8b3debaee893298574
genp <gen@cs.brown.edu>
2013-03-27 16:11:47 -0400

    updated extract_feature.m to save intermediate feature files

--- code/features/getLevelScales.m
+++
@@ -1,5 +0,0 @@
-function scales = getLevelScales(numLevels, interval)
-% Author: saurabh.me@gmail.com (Saurabh Singh).
-sc = 2^(1 / interval);
-scales = sc.^(0 : numLevels - 1);
-end
--- code/utils/doNmsForImg.m
+++
@@ -1,8 +0,0 @@
-function picks = doNmsForImg(data, decisionScore, overlap)
-% Do NMS for patches from a single image.
-%
-% Author: saurabh.me@gmail.com (Saurabh Singh).
-
-boxes = getBoxesForPedro(data, decisionScore);
-picks = myNms(boxes, overlap);
-end
